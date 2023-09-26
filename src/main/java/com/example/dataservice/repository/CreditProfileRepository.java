1fcec77a019ec9ef31587b790b8f5b6fb2f0427a
Dat Le <ledat1011>
2023-09-26 11:04:03 +0700

    update version

--- src/main/java/com/example/dataservice/repository/CreditProfileRepository.java
+++ src/main/java/com/example/dataservice/repository/CreditProfileRepository.java
@@ -3,10 +3,7 @@ package com.example.dataservice.repository;
 import java.util.Date;
 import java.util.List;
 
-import javax.transaction.Transactional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
